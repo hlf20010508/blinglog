9ce0e29887439f4d07332fcdb9d1771937df0f0d
hlf20010508 <hlf01@icloud.com>
2023-03-02 15:03:39 +0800

    更改域名

--- blog/static/js/foot.js
+++ blog/static/js/foot.js
@@ -3,7 +3,9 @@ let year = date.getFullYear()
 let element = document.getElementsByClassName('year')[0]
 element.textContent = year
 let host = location.protocol + '//' + document.domain + ':' + location.port
-if (host != 'http://124.223.224.49:5000') {
+element = document.getElementsByClassName('indexLink')[0]
+    element.setAttribute('target', '_blank')
+if (host != 'http://blog.hlf01.site' || host != 'http://hlf01.site') {
     element = document.getElementsByClassName('indexLink')[0]
     element.setAttribute('target', '_blank')
 }
