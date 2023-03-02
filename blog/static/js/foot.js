78ff924d77dad359f2259c2b1d765bcdfb69d7d4
hlf20010508 <hlf01@icloud.com>
2023-03-03 00:14:34 +0800

    底部去除主页链接

--- blog/static/js/foot.js
+++ blog/static/js/foot.js
@@ -2,10 +2,10 @@ let date = new Date()
 let year = date.getFullYear()
 let element = document.getElementsByClassName('year')[0]
 element.textContent = year
-let host = location.protocol + '//' + document.domain + ':' + location.port
-element = document.getElementsByClassName('indexLink')[0]
-    element.setAttribute('target', '_blank')
-if (host != 'http://blog.hlf01.site' || host != 'http://hlf01.site') {
-    element = document.getElementsByClassName('indexLink')[0]
-    element.setAttribute('target', '_blank')
-}
+// let host = location.protocol + '//' + document.domain + ':' + location.port
+// element = document.getElementsByClassName('indexLink')[0]
+//     element.setAttribute('target', '_blank')
+// if (host != 'http://blog.hlf01.site' || host != 'http://hlf01.site') {
+//     element = document.getElementsByClassName('indexLink')[0]
+//     element.setAttribute('target', '_blank')
+// }
