01fb7d8ff189c033fecabd25198d4f6135289786
L-ING <hlf01@icloud.com>
2023-11-09 11:14:44 +0800

    左下角角标与name和Links联动

--- blog/static/js/foot.js
+++ blog/static/js/foot.js
@@ -1,11 +1,4 @@
 let date = new Date()
 let year = date.getFullYear()
-let yearElement = document.getElementsByClassName('year')[0]
+let yearElement = document.getElementById('foot-year')
 yearElement.textContent = year
-// let host = location.protocol + '//' + document.domain + ':' + location.port
-// element = document.getElementsByClassName('indexLink')[0]
-//     element.setAttribute('target', '_blank')
-// if (host != 'http://blog.hlf01.site' || host != 'http://hlf01.site') {
-//     element = document.getElementsByClassName('indexLink')[0]
-//     element.setAttribute('target', '_blank')
-// }
